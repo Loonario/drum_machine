641445d6d5a7ffc3950d7cc2a0a82b92d6d53aab
Alexander Tarasov <alexloonatik@gmail.com>
2022-05-19 12:50:37 +0300

    doubble action on keyPress solved

--- src/components/SampleButton.js
+++ src/components/SampleButton.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import './SampleButton.css';
@@ -6,30 +6,66 @@ import './SampleButton.css';
 
 const SampleButton = (props) => {
     const {id, keyTrigger, keyCode, url} = props;
-    const [key, setKey] = useState();
+    const [padActive, setPadActive] = useState(false);
+    const [drumPadClass, setdrumPadClass] = useState('button-pad')
+    const [keyPressed, setKeyPressed] = useState('');
 
     useEffect(() => {
-        document.addEventListener('keydown', (event) => {
-            event.preventDefault();
-            if (event.key == keyTrigger.toLowerCase() && !event.repeat){
-                let sound = new Audio (url);
-                sound.currentTime = 0;
-                 sound.play();
+        document.addEventListener('keydown', handleKeyPress);
+            return () => {
+                document.removeEventListener('keydown', handleKeyPress)         
             }
-            })
-        
-    })
+    }, []);
+
+    const handleKeyPress = (e) => {
+        if (e.key == keyTrigger.toLowerCase() && !e.repeat){
+            //console.log(`key ${e.key} pressed`);
+            e.preventDefault();
+            playSound();
+            setKeyPressed(e.key);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keyup', handleKeyUp);
+            
+        return () => {
+            document.removeEventListener('keyup', handleKeyUp);
+        }
+    }, [keyPressed])
+
+    const handleKeyUp = (e) => {
+        if (keyPressed == keyTrigger.toLowerCase() && !e.repeat){
+            e.preventDefault();
+            //console.log(`key ${e.key} off`);
+            setKeyPressed('');
+            setdrumPadClass('button-pad');
+    }
+};
    
     const onDrumClick = () => {
+        //console.log(`Clicked`);
         let sound = new Audio (url);
+        sound.loop = false;
         sound.currentTime = 0;
+        sound.volume = 0.2;
         sound.play();
+      }
+
+      const playSound = () => {
+        let sound = new Audio(url);
+        sound.volume = 0.2;
+        sound.loop = false;
+        sound.currentTime = 0;
+     sound.play();
+     setdrumPadClass('button-pad-keypress');
+     
       }
    
 
     return(
         <div>
-           <div className = 'button-pad' onClick={onDrumClick}><div><b>{keyTrigger}</b></div></div>
+           <div className = {drumPadClass} onClick={onDrumClick}><div><b>{keyTrigger}</b></div></div>
         </div>
     )
 }
